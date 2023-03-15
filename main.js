6c014e087ef96068a0b02bd713ae2a3a192a4278
ahadsts9901 <ahadsts990@gmail.com>
2023-03-15 14:54:51 -0700

    done

--- main.js
+++ main.js
@@ -7,26 +7,44 @@ function add(event)
 
 
     let x1 = document.getElementById('x1').value
+        x1 = Number(x1)
     let x2 = document.getElementById('x2').value
+        x2 = Number(x2)
     let x3 = document.getElementById('x3').value
+        x3 = Number(x3)
     let x4 = document.getElementById('x4').value
+        x4 = Number(x4)
     let x5 = document.getElementById('x5').value
+        x5 = Number(x5)
     let x6 = document.getElementById('x6').value
+        x6 = Number(x6)
     let x7 = document.getElementById('x7').value
+        x7 = Number(x7)
     let x8 = document.getElementById('x8').value
+        x8 = Number(x8)
     let x9 = document.getElementById('x9').value
+        x9 = Number(x9)
 
     let FirstValues = [x1, x2, x3, x4, x5, x6, x7, x8, x9]
 
     let y1 = document.getElementById('y1').value
+        y1 = Number(y1)
     let y2 = document.getElementById('y2').value
+        y2 = Number(y2)
     let y3 = document.getElementById('y3').value
+        y3 = Number(y3)
     let y4 = document.getElementById('y4').value
+        y4 = Number(y4)
     let y5 = document.getElementById('y5').value
+        y5 = Number(y5)
     let y6 = document.getElementById('y6').value
+        y6 = Number(y6)
     let y7 = document.getElementById('y7').value
+        y7 = Number(y7)
     let y8 = document.getElementById('y8').value
+        y8 = Number(y8)
     let y9 = document.getElementById('y9').value
+        y9 = Number(y9)
 
     let SecondValues = [y1, y2, y3, y4, y5, y6, y7, y8, y9]
 
@@ -40,6 +58,163 @@ function add(event)
     z8 = FirstValues[7] + SecondValues[7]
     z9 = FirstValues[8] + SecondValues[8]
 
-    console.log (z1)
+    document.getElementById('z1').innerHTML = z1
+    document.getElementById('z2').innerHTML = z2
+    document.getElementById('z3').innerHTML = z3
+    document.getElementById('z4').innerHTML = z4
+    document.getElementById('z5').innerHTML = z5
+    document.getElementById('z6').innerHTML = z6
+    document.getElementById('z7').innerHTML = z7
+    document.getElementById('z8').innerHTML = z8
+    document.getElementById('z9').innerHTML = z9
+    
+}
+
+// matrix subtract
+
+function subtract(event)
+{
+
+    {
+        event.preventDefault()
+    }
+
+    let x1 = document.getElementById('x1').value
+        x1 = Number(x1)
+    let x2 = document.getElementById('x2').value
+        x2 = Number(x2)
+    let x3 = document.getElementById('x3').value
+        x3 = Number(x3)
+    let x4 = document.getElementById('x4').value
+        x4 = Number(x4)
+    let x5 = document.getElementById('x5').value
+        x5 = Number(x5)
+    let x6 = document.getElementById('x6').value
+        x6 = Number(x6)
+    let x7 = document.getElementById('x7').value
+        x7 = Number(x7)
+    let x8 = document.getElementById('x8').value
+        x8 = Number(x8)
+    let x9 = document.getElementById('x9').value
+        x9 = Number(x9)
+
+    let FirstValues = [x1, x2, x3, x4, x5, x6, x7, x8, x9]
+
+    let y1 = document.getElementById('y1').value
+        y1 = Number(y1)
+    let y2 = document.getElementById('y2').value
+        y2 = Number(y2)
+    let y3 = document.getElementById('y3').value
+        y3 = Number(y3)
+    let y4 = document.getElementById('y4').value
+        y4 = Number(y4)
+    let y5 = document.getElementById('y5').value
+        y5 = Number(y5)
+    let y6 = document.getElementById('y6').value
+        y6 = Number(y6)
+    let y7 = document.getElementById('y7').value
+        y7 = Number(y7)
+    let y8 = document.getElementById('y8').value
+        y8 = Number(y8)
+    let y9 = document.getElementById('y9').value
+        y9 = Number(y9)
+
+    let SecondValues = [y1, y2, y3, y4, y5, y6, y7, y8, y9]
+
+    z1 = FirstValues[0] - SecondValues[0]
+    z2 = FirstValues[1] - SecondValues[1]
+    z3 = FirstValues[2] - SecondValues[2]
+    z4 = FirstValues[3] - SecondValues[3]
+    z5 = FirstValues[4] - SecondValues[4]
+    z6 = FirstValues[5] - SecondValues[5]
+    z7 = FirstValues[6] - SecondValues[6]
+    z8 = FirstValues[7] - SecondValues[7]
+    z9 = FirstValues[8] - SecondValues[8]
+
+    document.getElementById('z1').innerHTML = z1
+    document.getElementById('z2').innerHTML = z2
+    document.getElementById('z3').innerHTML = z3
+    document.getElementById('z4').innerHTML = z4
+    document.getElementById('z5').innerHTML = z5
+    document.getElementById('z6').innerHTML = z6
+    document.getElementById('z7').innerHTML = z7
+    document.getElementById('z8').innerHTML = z8
+    document.getElementById('z9').innerHTML = z9
+    
+
+}
+
+// matrix multiply
+
+
+function multiply(event)
+{
+
+    {
+        event.preventDefault()
+    }
+
+
+    let x1 = document.getElementById('x1').value
+        x1 = Number(x1)
+    let x2 = document.getElementById('x2').value
+        x2 = Number(x2)
+    let x3 = document.getElementById('x3').value
+        x3 = Number(x3)
+    let x4 = document.getElementById('x4').value
+        x4 = Number(x4)
+    let x5 = document.getElementById('x5').value
+        x5 = Number(x5)
+    let x6 = document.getElementById('x6').value
+        x6 = Number(x6)
+    let x7 = document.getElementById('x7').value
+        x7 = Number(x7)
+    let x8 = document.getElementById('x8').value
+        x8 = Number(x8)
+    let x9 = document.getElementById('x9').value
+        x9 = Number(x9)
+
+    let FirstValues = [x1, x2, x3, x4, x5, x6, x7, x8, x9]
+
+    let y1 = document.getElementById('y1').value
+        y1 = Number(y1)
+    let y2 = document.getElementById('y2').value
+        y2 = Number(y2)
+    let y3 = document.getElementById('y3').value
+        y3 = Number(y3)
+    let y4 = document.getElementById('y4').value
+        y4 = Number(y4)
+    let y5 = document.getElementById('y5').value
+        y5 = Number(y5)
+    let y6 = document.getElementById('y6').value
+        y6 = Number(y6)
+    let y7 = document.getElementById('y7').value
+        y7 = Number(y7)
+    let y8 = document.getElementById('y8').value
+        y8 = Number(y8)
+    let y9 = document.getElementById('y9').value
+        y9 = Number(y9)
+
+    let SecondValues = [y1, y2, y3, y4, y5, y6, y7, y8, y9]
+
+    z1 = (FirstValues[0]*SecondValues[0]) + (FirstValues[1]*SecondValues[3]) + (FirstValues[2]*SecondValues[6])
+    z2 = (FirstValues[0]*SecondValues[1]) + (FirstValues[1]*SecondValues[4]) + (FirstValues[2]*SecondValues[7])
+    z3 = (FirstValues[0]*SecondValues[2]) + (FirstValues[1]*SecondValues[5]) + (FirstValues[2]*SecondValues[8])
+    z4 = (FirstValues[3]*SecondValues[0]) + (FirstValues[4]*SecondValues[3]) + (FirstValues[5]*SecondValues[6])
+    z5 = (FirstValues[3]*SecondValues[1]) + (FirstValues[4]*SecondValues[4]) + (FirstValues[5]*SecondValues[7])
+    z6 = (FirstValues[3]*SecondValues[2]) + (FirstValues[4]*SecondValues[5]) + (FirstValues[5]*SecondValues[8])
+    z7 = (FirstValues[6]*SecondValues[0]) + (FirstValues[7]*SecondValues[3]) + (FirstValues[8]*SecondValues[6])
+    z8 = (FirstValues[6]*SecondValues[1]) + (FirstValues[7]*SecondValues[4]) + (FirstValues[8]*SecondValues[7])
+    z9 = (FirstValues[6]*SecondValues[2]) + (FirstValues[7]*SecondValues[5]) + (FirstValues[8]*SecondValues[8])
+
+    document.getElementById('z1').innerHTML = z1
+    document.getElementById('z2').innerHTML = z2
+    document.getElementById('z3').innerHTML = z3
+    document.getElementById('z4').innerHTML = z4
+    document.getElementById('z5').innerHTML = z5
+    document.getElementById('z6').innerHTML = z6
+    document.getElementById('z7').innerHTML = z7
+    document.getElementById('z8').innerHTML = z8
+    document.getElementById('z9').innerHTML = z9
     
 }
