c6659abc26f0660d9674b4f70d9303ac19dbc010
ahadsts9901 <ahadsts990@gmail.com>
2023-03-15 13:52:39 -0700

    submit refresh issue

---
+++ main.js
@@ -0,0 +1,45 @@
+function add( event )
+{
+
+    {
+        event.preventDefault()
+    }
+
+
+    let x1 = document.getElementById('x1').value
+    let x2 = document.getElementById('x2').value
+    let x3 = document.getElementById('x3').value
+    let x4 = document.getElementById('x4').value
+    let x5 = document.getElementById('x5').value
+    let x6 = document.getElementById('x6').value
+    let x7 = document.getElementById('x7').value
+    let x8 = document.getElementById('x8').value
+    let x9 = document.getElementById('x9').value
+
+    let FirstValues = [x1, x2, x3, x4, x5, x6, x7, x8, x9]
+
+    let y1 = document.getElementById('y1').value
+    let y2 = document.getElementById('y2').value
+    let y3 = document.getElementById('y3').value
+    let y4 = document.getElementById('y4').value
+    let y5 = document.getElementById('y5').value
+    let y6 = document.getElementById('y6').value
+    let y7 = document.getElementById('y7').value
+    let y8 = document.getElementById('y8').value
+    let y9 = document.getElementById('y9').value
+
+    let SecondValues = [y1, y2, y3, y4, y5, y6, y7, y8, y9]
+
+    let z1 = FirstValues[0] + SecondValues[0]
+    // let z2 =
+    // let z3 =
+    // let z4 =
+    // let z5 =
+    // let z6 =
+    // let z7 =
+    // let z8 =
+    // let z9 =
+
+    console.log (z1)
+    
+}
